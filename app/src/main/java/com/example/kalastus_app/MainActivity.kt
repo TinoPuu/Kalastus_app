28f6f699d52ae9c138c1df01beebafaf7ea8e1e1
AkuOE <akutonteri@gmail.com>
2021-11-12 11:09:41 +0200

    Fragmentit tehty kahdelle näkymälle, liikkuminen ei toimi vielä.

--- app/src/main/java/com/example/kalastus_app/MainActivity.kt
+++ app/src/main/java/com/example/kalastus_app/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.kalastus_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +11,19 @@ class MainActivity : AppCompatActivity() {
         //tässä testi :DDD
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val firstFragment = FirstFragment()
+        val secondFragment = SecondFragment()
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, firstFragment)
+            commit()
+        }
+
+        "btnFragment1.setOnClickListener{}"
+
+
+
+
     }
 }

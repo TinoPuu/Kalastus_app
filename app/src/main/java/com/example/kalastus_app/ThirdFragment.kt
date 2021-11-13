4474ddcac332ccb6c0fc437488dfc6948335dc68
TinoPuu <t1n0.p22stinen@gmail.com>
2021-11-13 14:09:38 +0200

    Kalavitsit toimivat  13.11.2021 14.09

--- app/src/main/java/com/example/kalastus_app/ThirdFragment.kt
+++ app/src/main/java/com/example/kalastus_app/ThirdFragment.kt
@@ -3,6 +3,7 @@ package com.example.kalastus_app
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log.d
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.TextureView
@@ -13,7 +14,10 @@ import android.widget.TextView
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import org.w3c.dom.Element
 import org.w3c.dom.Text
+import java.util.*
+import kotlin.random.Random
 
 class ThirdFragment : Fragment() {
 
@@ -22,8 +26,32 @@ class ThirdFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val db = Firebase.firestore
         val view = inflater.inflate(R.layout.fragment_third, container,false)
+        val tvKalavale : TextView = view.findViewById(R.id.tvKalavale)
+        val tvKalavitsi : TextView = view.findViewById(R.id.tvKalavitsi)
+        val btnKalavale : Button = view.findViewById(R.id.btnKalavale)
+        val btnKalavitsi : Button = view.findViewById(R.id.btnKalavitsi)
         val btnTakaisin : Button = view.findViewById(R.id.buttonTakaisinKalavale)
+
+        btnKalavitsi.setOnClickListener{
+            val number = Random.nextInt(1,11)
+            d("jpk",number.toString())
+            db.collection("Kalavitsit").document(number.toString())
+                .get()
+
+                .addOnSuccessListener { result ->
+                    val vitsi = result.data.toString()
+                    tvKalavitsi.text = vitsi.removePrefix("{Vitsi=").removeSuffix("}")
+
+                }
+                .addOnFailureListener { exception ->
+                    tvKalavitsi.text = "error"
+
+                }
+        }
+
+
         btnTakaisin.setOnClickListener {
             val fragment = FourthFragment()
             val transaction = fragmentManager?.beginTransaction()

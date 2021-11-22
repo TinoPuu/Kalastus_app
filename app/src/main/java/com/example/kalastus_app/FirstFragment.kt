b73af8c3657536ae6ab45593c2ed0219d293bb03
Attoss <atte.tossavainen@edu.savonia.fi>
2021-11-22 10:58:51 +0200

    Kehitelty kalanlisämissivua ja tehty muutamia vaihtoehtoja

--- app/src/main/java/com/example/kalastus_app/FirstFragment.kt
+++ app/src/main/java/com/example/kalastus_app/FirstFragment.kt
@@ -1,17 +1,22 @@
 package com.example.kalastus_app
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.PopupMenu
+import androidx.fragment.app.Fragment
 
 class FirstFragment : Fragment() {
+
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
         val view = inflater.inflate(R.layout.fragment_first, container,false)
         val btnTakaisin : Button = view.findViewById(R.id.buttonTakaisinKalavalikko)
@@ -20,11 +25,75 @@ class FirstFragment : Fragment() {
             val transaction = fragmentManager?.beginTransaction()
             transaction?.replace(R.id.flFragment,fragment)?.commit()
         }
-        //tästä napista avautuu kalan lisäys kohta
-        val btnLisaakala : Button = view.findViewById(R.id.buttonLisääkala)
-        btnLisaakala.setOnClickListener {
 
+        val btnLisaakala : Button = view.findViewById(R.id.btnLisaakala)
+        btnLisaakala.setOnClickListener {
+            val popupMenu: PopupMenu = PopupMenu(requireContext(),btnLisaakala)
+            popupMenu.menuInflater.inflate(R.menu.popup_kalat,popupMenu.menu)
+            popupMenu.setOnMenuItemClickListener(PopupMenu.OnMenuItemClickListener { item ->
+                when(item.itemId) {
+                    R.id.action_ahven ->
+                        btnLisaakala.text = "Ahven"
+                    R.id.action_hauki ->
+                        btnLisaakala.text = "Hauki"
+                    R.id.action_kuha ->
+                        btnLisaakala.text = "Kuha"
+                    R.id.action_siika ->
+                        btnLisaakala.text = "Siika"
+                    R.id.action_sayne ->
+                        btnLisaakala.text = "Sayne"
+                    R.id.action_aki ->
+                        btnLisaakala.text = "Aki"
+                }
+
+                true
+            })
+            popupMenu.show()
         }
+        val btnTapa : Button = view.findViewById(R.id.btnTapa)
+        btnTapa.setOnClickListener {
+            val popupMenu: PopupMenu = PopupMenu(requireContext(),btnTapa)
+            popupMenu.menuInflater.inflate(R.menu.popup_kalastustavat,popupMenu.menu)
+            popupMenu.setOnMenuItemClickListener(PopupMenu.OnMenuItemClickListener { item ->
+                when(item.itemId) {
+                    R.id.action_jigi ->
+                        btnTapa.text = "Jigi"
+                    R.id.action_perho ->
+                        btnTapa.text = "Perho"
+                    R.id.action_uistin ->
+                        btnTapa.text = "Uistin"
+                    R.id.action_verkko ->
+                        btnTapa.text = "Verkko"
+                    R.id.action_pilkki ->
+                        btnTapa.text = "Pilkki"
+
+                }
+
+                true
+            })
+            popupMenu.show()
+        }
+        val btnPaikka : Button = view.findViewById(R.id.btnPaikka)
+        btnPaikka.setOnClickListener {
+            val popupMenu: PopupMenu = PopupMenu(requireContext(),btnPaikka)
+            popupMenu.menuInflater.inflate(R.menu.popup_kalastuspaikka,popupMenu.menu)
+            popupMenu.setOnMenuItemClickListener(PopupMenu.OnMenuItemClickListener { item ->
+                when(item.itemId) {
+                    R.id.action_lampi ->
+                        btnPaikka.text = "Lampi"
+                    R.id.action_järvi ->
+                        btnPaikka.text = "Järvi"
+                    R.id.action_joki ->
+                        btnPaikka.text = "Joki"
+
+
+                }
+
+                true
+            })
+            popupMenu.show()
+        }
+        val Paino : (EditText) = view.findViewById(R.id.Paino)
 
       return view
     }
